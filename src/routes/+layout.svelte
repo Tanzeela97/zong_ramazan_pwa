d4ea1c62bf91f40dd2afa8f332041c37e2b2818d
Emran Riyaz <iariyaz@vxt.com>
2023-03-24 00:08:36 +0500

    WIP: moved app.css reference from index.html to import in +layout.svelte
    It causes problems in production build, bundler couldnt locate it
    wref index.html

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+  import "../app.css";
   //   export const ssr = false;
   import NavBar from "../components/NavBar/page.svelte";
-  import NotificationBar from "../components/NotificationBar/page.svelte";
-  import { city } from "../stores/store";
-  import { CITIES_LIST, type TCityData } from "../data/constants";
+  //   import NotificationBar from "../components/NotificationBar/page.svelte";
+  //   import { city } from "../stores/store";
+  //   import { CITIES_LIST, type TCityData } from "../data/constants";
 
   function handleSelect(e: Event) {
     // const selectedCity = e.target;
